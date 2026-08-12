526f0b395bcafdec90a3be0719f8c0a9151c9482
JonathanJHK <jonathanheidyjhk@gmail.com>
2026-08-12 13:05:47 -0300

    ✨ feat(api): initialize web api project
      - initialize solution and gerenciadorpedido.api project
      - add entity framework core postgresql and swagger packages
      - configure openapi documentation and swagger ui middleware

---
+++ src/GerenciadorPedido.Api/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    // Adicionando o Swagger UI para visualização da documentação
+    app.UseSwaggerUI(options =>
+    {   
+        // Configurando o endpoint do Swagger UI para acessar a documentação da API
+        options.SwaggerEndpoint("/openapi/v1.json", "Gerenciador de Pedidos API v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
